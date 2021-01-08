95d3be20b61b666d99968f6bd8f5ca8f34ca34ef
abraham santosa <abraham.santosa.67@my.csun.edu>
2021-01-08 15:16:26 +0700

    delete pending works

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+
 const db = {
   professors: [
     {
@@ -27,13 +28,49 @@ const db = {
         "How do you even pass this class?",
         "Hey I got a B!",
       ],
-      ratings: [3, 5, 3, 4],
+      ratings: [2, 2, 4, 4],
+    },
+  ],
+  users: [
+    {
+      email: "john",
+      password: "cookies",
+      isAdmin: true,
+    },
+  ],
+  pending: [
+    {
+      id: "test",
+      name: "jasdksjlkdsa",
+      university: "bblblblee",
+      comments: ["test1"],
+      ratings: [2],
+    },
+    {
+      id: "test2",
+      name: "ajskldfs",
+      university: "asfds",
+      comments: ["test2"],
+      ratings: [4],
     },
   ],
 };
 
+app.get("/signin", (req, res) => {
+  const { email, password } = req.body;
+  db.map.users((user) => {
+    if (user.email == email) {
+      if (user.password == password) res.send(user);
+    }
+  });
+});
+
 app.get("/home", (req, res) => {
   res.send(db.professors);
+});
+
+app.get("/pending", (req, res) => {
+  res.send(db.pending);
 });
 
 app.post("/addprofessor", (req, res) => {
@@ -51,14 +88,31 @@ app.post("/addprofessor", (req, res) => {
 });
 
 app.put("/updatereview", (req, res) => {
-  const { id, name, university, comments, ratings } = req.body;
+  const { id, comments, ratings } = req.body;
 
-  db.professors.map(prof, (req, res) => {
+  db.professors.map((prof) => {
     if (prof.id == id) {
       prof.comments.push(comments);
       prof.ratings.push(ratings);
     }
   });
+});
+
+app.get("/deletePending/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  let deleted = false;
+  db.pending.map(function (user, index) {
+    if (user.id == id) {
+      db.pending.splice(index, 1);
+      deleted = true;
+    }
+  });
+  if (!deleted) {
+    console.log("not found");
+  } else {
+    console.log("success");
+  }
 });
 
 app.listen(3000, () => {
